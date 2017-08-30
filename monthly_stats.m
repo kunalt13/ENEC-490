156eb065222297418fe179e2c7329948ab4c1314
kunalt13 <31254722+kunalt13@users.noreply.github.com>
2017-08-30 00:40:02 -0400

    Add files via upload
    This is lecture 3 assignment with the monthly_stats function included

--- monthly_stats.m
+++ monthly_stats.m
@@ -3,21 +3,24 @@ function [ n ] = monthly_stats( vector )
 
 
 new_years = 1997:1:2016;
-start = find(new_years>=2008);
+start = new_years>=2008;
 
 n = vector(:,start);
 
-n = zeros(12, 2);
+p = zeros(12, 2);
 
+a=1;
 for i = 1
     for j = 1:12
-        n(j,i) = mean(vector)
+        p(i,j) = mean(n(a));
+        a=a+12;
     end
 end
-
+b=1;
 for i = 2
-    for j = 1:months
-        n(j,i) = std(vector)
+    for j = 1:12
+        n(j,i) = std(n(b));
+        b=b+12;
     end
 end
 
