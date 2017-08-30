e091f8d2fc40dd619aacea1551277f7173cf5858
Jordan Kern <jdkern@live.unc.edu>
2017-08-29 22:18:30 -0400

    Update Lecture2Kunal.m

--- Lecture2Kunal.m
+++ Lecture2Kunal.m
@@ -2,9 +2,11 @@
 
 years = floor(length(data)/12);
 yearly_average = zeros(years,1);
+
+%shouldn't start at 1 unless you're changing the .xls file beforehand
     a=1;
     b=12;
- for j= years
+ for j= 1:years
         yearly_average(j,1) = mean(data(a:b));
         a=a+12;
         b=b+12;
