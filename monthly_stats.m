97362e3ff3d29d972066a145b1c8774a081e2853
Jordan Kern <jdkern@live.unc.edu>
2017-09-01 16:47:40 -0400

    Update monthly_stats.m

--- monthly_stats.m
+++ monthly_stats.m
@@ -1,15 +1,11 @@
-function [ n ] = monthly_stats( d )
+function [ p ] = monthly_stats( d )
 %function that creates monthly gas prices to annual profile
 
 p = zeros(12, 2);
 
-a=1;
-b=1;
 for j = 1:12
-        p(j,1) = mean(d(a));
-        p(j,2) = std(d(b));
-        a=a+12;
-        b=b+12;
+        p(j,1) = mean(d(j,:));
+        p(j,2) = std(d(j,:));
 end
 
 
